eab6bbfd8c095208a36c73a7c42ab9633d1977aa
nataly <angynk17@gmail.com>
2018-02-05 21:59:25 -0500

    aDD Cloud configuration

--- src/main/java/Survey/Api/controller/settings/HibernateConfiguration.java
+++ src/main/java/Survey/Api/controller/settings/HibernateConfiguration.java
@@ -18,7 +18,7 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "Survey.Api")
-@PropertySource(value = { "classpath:application.properties" })
+@PropertySource(value = { "classpath:cloudDb.properties" })
 public class HibernateConfiguration {
 
     @Autowired
@@ -45,9 +45,9 @@ public class HibernateConfiguration {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+//        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
         return properties;
     }
 
