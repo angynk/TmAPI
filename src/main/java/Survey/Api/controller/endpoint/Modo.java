c9ff544dac7b4d4d52143cdd4af5757a6510488a
angynk17@gmail.com <capitulo3>
2018-03-07 10:21:39 -0500

    Incluir configuracion base para Zonal y troncal OD

--- src/main/java/Survey/Api/controller/endpoint/Modo.java
+++ src/main/java/Survey/Api/controller/endpoint/Modo.java
@@ -3,7 +3,7 @@ package Survey.Api.controller.endpoint;
 public class Modo {
 
     public static String TRONCAL = "tro";
-    public static String TRONCAL_ORIGEN_DESTINO = "tro";
+    public static String TRONCAL_ORIGEN_DESTINO = "tro-od";
     public static String ALIMENTACION = "ali";
     public static String ZONAL = "zon";
 }
